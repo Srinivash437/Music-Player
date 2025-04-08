f6dd8bb3fca62f866aa11d9bb856ac1b6f0b4f0a
Srinivash437 <146741598+Srinivash437@users.noreply.github.com>
2025-04-08 18:59:17 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,59 @@
+// Array of music track objects used in the music player
+let allMusic = [
+  {
+    // Song: Welcome To Hyderabad from Premalu
+    name: "Welcome To Hyderabad | Premalu",
+    artist: "Vishnu Vijay",
+    img: "music - 1", // Image filename (without extension)
+    src: "music - 1", // Audio source filename (without extension)
+  },
+  {
+    // Song: Vazhithunaiye from Dragon
+    name: "Vazhithunaiye | Dragon",
+    artist: "Leon James",
+    img: "music - 2",
+    src: "music - 2",
+  },
+  {
+    // Song: Anbil Avan from Vinnaithandi Varuvaya
+    name: "Anbil Avan | Vinnaithandi Varuvaya",
+    artist: "A.R.Rahman",
+    img: "music - 3",
+    src: "music - 3",
+  },
+  {
+    // Song: Adiye from Bachelor
+    name: "Adiye | Bachelor",
+    artist: "G.V.Prakash",
+    img: "music - 4",
+    src: "music - 4",
+  },
+  {
+    // Song: Malare Ninne from Premam
+    name: "Malare Ninne | Premam",
+    artist: "Rajesh Murugesan",
+    img: "music - 5",
+    src: "music - 5",
+  },
+  {
+    // Song: Thuli Thuli Mazhaiyaai from Paiyaa
+    name: "Thuli Thuli Mazhaiyaai | Paiyaa",
+    artist: "Yuvan Sankar Raja",
+    img: "music - 6",
+    src: "music - 6",
+  },
+  {
+    // Song: Hey Minnale from Amaran
+    name: "Hey Minnale | Amaran",
+    artist: "G.V.Prakash",
+    img: "music - 7",
+    src: "music - 7",
+  },
+  {
+    // Song: Golden Sparrow from Nilavuku En Mel Ennadi Kobam
+    name: "Golden Sparrow | Nilavuku En Mel Ennadi Kobam",
+    artist: "G.V.Prakash",
+    img: "music - 8",
+    src: "music - 8",
+  },
+];
